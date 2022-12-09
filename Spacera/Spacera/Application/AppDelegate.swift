45933878ad0114efd48bc58f2a5cd57338c669b8
artembilyy <incomecars4u@gmail.com>
2022-12-10 00:07:58 +0100

    Init commit.

---
+++ Spacera/Spacera/Application/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Spacera
+//
+//  Created by Artem Bilyi on 09.12.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    private let controller = ViewController()
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .white
+        let navigationController = UINavigationController(rootViewController: controller)
+        window?.rootViewController = navigationController
+        return true
+    }
+}
+
